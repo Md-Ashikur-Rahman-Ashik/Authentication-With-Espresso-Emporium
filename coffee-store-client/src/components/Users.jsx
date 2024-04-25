484b0f6c90993c42823e15ff7339d2d5804d0632
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-25 16:03:39 +0600

    Deleted a user from client side and database

--- coffee-store-client/src/components/Users.jsx
+++ coffee-store-client/src/components/Users.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
-
+  const [users, setUsers] = useState(loadedUsers);
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          console.log("Deleted Successfully");
+          //   Remove the user from the UI
+          const remainingUser = users.filter((user) => user._id !== id);
+          setUsers(remainingUser);
+        }
+      });
+  };
   return (
     <div>
-      <h2>Users: {loadedUsers.length}</h2>
+      <h2>Users: {users.length}</h2>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
@@ -14,17 +29,26 @@ const Users = () => {
               <th></th>
               <th>Email</th>
               <th>Created At</th>
+              <th>Last Logged In</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            {loadedUsers.map((user) => (
+            {users.map((user) => (
               <tr key={user._id}>
                 <th>1</th>
                 <td>{user.email}</td>
                 <td>{user.createdAt}</td>
-                <td>Blue</td>
+                <td></td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn"
+                  >
+                    Remove
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
