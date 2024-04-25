d480e614625e638e892601bebeb45eb25ced6e0f
MD ASHIKUR RAHMAN ASHIK <152707760+Md-Ashikur-Rahman-Ashik@users.noreply.github.com>
2024-04-25 03:33:38 +0000

    Added client resources

---
+++ coffee-store-client/src/components/CoffeeCard.jsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+  const { _id, name, quantity, supplier, taste, photo } =
+    coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+              const remaining = coffees.filter((cof) => cof._id !== _id);
+              setCoffees(remaining);
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div className="card card-side bg-base-100 shadow-xl gap-4 p-4">
+      <figure className="w-1/2">
+        <img src={photo} className="rounded-xl" alt="Movie" />
+      </figure>
+      <div className="flex justify-between w-full">
+        <div>
+          <h2 className="card-title">Name: {name}</h2>
+          <p>{quantity}</p>
+          <p>{supplier}</p>
+          <p>{taste}</p>
+        </div>
+        <div className="card-actions justify-end">
+          <div className="join join-vertical space-y-4">
+            <button className="btn">View</button>
+            <Link to={`/update-coffee/${_id}`}>
+              <button className="btn">Edit</button>
+            </Link>
+            <button
+              className="btn text-center bg-red-500 text-white"
+              onClick={() => handleDelete(_id)}
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
