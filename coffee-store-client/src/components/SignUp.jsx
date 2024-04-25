0ae4b30932a093ff9c42452360f47b3fc79d38ef
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-25 11:45:52 +0600

    Sign Up, Sign In, Firebase, Context Recap

--- coffee-store-client/src/components/SignUp.jsx
+++ coffee-store-client/src/components/SignUp.jsx
@@ -1,9 +1,65 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
 const SignUp = () => {
-    return (
-        <div>
-            <h2>Please Sign Up</h2>            
+  const { createUser } = useContext(AuthContext);
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log("The email is:", email);
+    console.log("The password is:", password);
+
+    createUser(email, password).then((result) => {
+      console.log(result.user);
+    //   New user has been created
+    });
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Please Sign Up!</h1>
         </div>
-    );
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body" onSubmit={handleSignUp}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Sign Up</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
